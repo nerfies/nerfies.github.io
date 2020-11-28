3053453a1ce107a5a85aa468a0878a282e4377ee
Keunhong Park <kpar@cs.washington.edu>
2020-11-28 02:45:31 -0800

    Add navbar.

--- static/js/index.js
+++ static/js/index.js
@@ -21,6 +21,14 @@ function setInterpolationImage(i) {
 
 
 $(document).ready(function() {
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+    });
+
     var options = {
 			slidesToScroll: 1,
 			slidesToShow: 3,

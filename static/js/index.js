10de061ca95c1453fb7b3caac4e51877ff1b8c6f
Keunhong Park <kpar@cs.washington.edu>
2022-12-16 19:21:57 -0800

    Fix videos and images.

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "https://homes.cs.washington.edu/~kpar/nerfies/interpolation/stacked";
+var INTERP_BASE = "./static/interpolation/stacked";
 var NUM_INTERP_FRAMES = 240;
 
 var interp_images = [];
